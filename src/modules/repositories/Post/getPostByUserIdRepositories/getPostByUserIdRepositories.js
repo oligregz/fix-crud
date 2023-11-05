812ca5f539ee963dc0065c0c14180df9333b866c
oligregz <gregorysak90@gmail.com>
2023-11-05 13:00:32 -0300

    feat/fix: add get post by user id route and change return method in repository from forEach to map

--- src/modules/repositories/Post/getPostByUserIdRepositories/getPostByUserIdRepositories.js
+++ src/modules/repositories/Post/getPostByUserIdRepositories/getPostByUserIdRepositories.js
@@ -1,26 +1,22 @@
 const { 
     client
-} = require('../../../common/handlers')
+} = require('../../../common/handlers');
 
 const getPostByUserIdRepositories = async ({
     user_id
 } = {}) => {
 
-    const response = await client('posts').where({author_id: user_id})
+    const response = await client('posts').where({author_id: user_id});
 
     const has_response = Array.isArray(response) && response.length > 0;
 
     if(!has_response){
-        return {
-            posts: []
-        }
+        return posts = [];
     }
 
-    const posts = response.forEach(post => post);
+    const posts = response.map((post) => post);
 
-    return {
-        posts
-    }
+    return posts;
 
 }
 
--- src/modules/services/Post/listPostService/listPostService.js
+++ src/modules/services/Post/listPostService/listPostService.js
@@ -1,31 +1,19 @@
 const { getUserByIdService } = require("../../User/getUserByIdService/getUserByIdService");
 const { getPostByUserIdRepositories } = require("../../../repositories");
 
-const getPostByUserIdService = async ({
-    user_id
-}) => {
+const getPostByUserIdService = async ({ user_id }) => {
 
-    const {
-        user
-    } = await getUserByIdService({
-        user_id
-    })
+    const user = await getUserByIdService({ user_id });
 
     const has_author = Array.isArray(user) && user.length > 0;
 
-    if(has_author === false) {
-        throw new Error("Missing author in database")
+    if(!has_author) {
+        throw new Error("Missing author in database");
     }
 
-    const {
-        posts = []
-    } = await getPostByUserIdRepositories({
-        user_id
-    });
+    const posts = await getPostByUserIdRepositories({ user_id });
 
-    return {
-        posts
-    };
+    return posts;
 }
 
 module.exports = {
