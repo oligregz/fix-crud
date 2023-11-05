4a38a34a5386056d58d36281c0fce115f602adc4
oligregz <gregorysak90@gmail.com>
2023-11-05 17:05:43 -0300

    feat: add adds logic for searching posts by id

--- src/modules/handlers/Posts/listPost.js
+++ src/modules/handlers/Posts/listPost.js
@@ -6,12 +6,31 @@ const { getPostByUserIdService } = require('../../services');
 const listPostHandler = async (req, res, next) => {
     try{
         const {
-            user_id
+            user_id,
+            post_id
         } = req.query;
-        
-        const posts = await getPostByUserIdService({ user_id: user_id });
 
-        return res.status(httpStatusCodes.StatusCodes.OK).send({ posts });
+        // console.log('user_id: ', user_id)
+        // console.log('post_id: ', post_id)
+
+        const has_user_id = typeof user_id === 'string' && !isNaN(user_id);
+        const has_post_id = typeof post_id === 'string' && !isNaN(post_id);
+        // console.log('has_user_id: ', has_user_id)
+        // console.log('has_post_id: ', has_post_id)
+
+        if (has_user_id) {
+            // console.log('user_id:', user_id)
+            const posts = await getPostByUserIdService({ user_id: user_id });
+    
+            return res.status(httpStatusCodes.StatusCodes.OK).send({ posts });
+
+        } else if (has_post_id) {
+            // console.log('post_id:', post_id)
+            const posts = await getPostByUserIdService({ post_id: post_id });
+
+            return res.status(httpStatusCodes.StatusCodes.OK).send({ posts });
+        }
+        
 
     }catch(error){
         return httpErrorHandler({ req, res, error })
--- src/modules/services/Post/listPostService/listPostService.js
+++ src/modules/services/Post/listPostService/listPostService.js
@@ -1,19 +1,38 @@
 const { getUserByIdService } = require("../../User/getUserByIdService/getUserByIdService");
 const { getPostByUserIdRepositories } = require("../../../repositories");
+const { getPostByPostIdRepositories } = require("../../../repositories");
 
-const getPostByUserIdService = async ({ user_id }) => {
+const getPostByUserIdService = async ({ user_id, post_id }) => {
 
-    const user = await getUserByIdService({ user_id });
+    const has_user_id = typeof user_id === 'string' && !isNaN(user_id);
+    const has_post_id = typeof post_id === 'string' && !isNaN(post_id);
 
-    const has_author = Array.isArray(user) && user.length > 0;
+    if (has_user_id) {
+        const user = await getUserByIdService({ user_id });
 
-    if(!has_author) {
-        throw new Error("Missing author in database");
+        const has_author = Array.isArray(user) && user.length > 0;
+    
+        if(!has_author) {
+            throw new Error("Missing author in database");
+        }
+    
+        const posts = await getPostByUserIdRepositories({ user_id });
+    
+        return posts;
+
+    } else if (has_post_id) {
+        
+        const posts = await getPostByPostIdRepositories({ post_id });
+
+        const has_posts = Array.isArray(posts) && posts.length > 0;
+
+        if(!has_posts) {
+            throw new Error("Missing posts in database");
+        }
+
+        return posts;
     }
-
-    const posts = await getPostByUserIdRepositories({ user_id });
-
-    return posts;
+    throw new Error("Missing id key");
 }
 
 module.exports = {
