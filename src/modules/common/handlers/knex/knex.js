7c00140b172dbfd8ed057e6632758814457bb127
oligregz <gregorysak90@gmail.com>
2023-11-06 21:45:11 -0300

    feat: add validation for connection

--- src/modules/common/handlers/knex/knex.js
+++ src/modules/common/handlers/knex/knex.js
@@ -18,6 +18,23 @@ const getTransaction = async () => {
     return {transaction};
 }
 
+const testDatabaseConnection = async () => {
+  try {
+    const result = await knex.raw('SELECT 1 as result');
+    const firstRow = result.rows[0];
+    
+    if (firstRow && 'result' in firstRow) {
+      console.log('Connection creaed with database, result: ', firstRow.result);
+    } else {
+      console.error('Bad connection');
+    }
+  } catch (error) {
+    console.error('Erro connect database:', error);
+  } finally {
+    await knex.destroy();
+  }
+}
+
 const commitTransaction = ({ transaction }) => transaction.commit();
 
 const rollbackTransaction = ({ transaction }) => transaction.rollback();
